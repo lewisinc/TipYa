134ab1642c9c16c8479c5a88a11810baa9ebf38e
David Lewis <davidlewis@icloud.com>
2015-04-04 06:16:20 -0700

    Added some identity files to our project. Note to future self: encrypt stuff that needs to be.

--- TipYa/SpectateOnPerformerViewController.swift
+++ TipYa/SpectateOnPerformerViewController.swift
@@ -21,7 +21,15 @@ class SpectateOnPerformerViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    func addPerformerToTableView(title:String!, image:UIImage?) -> Bool {
+        /* Adds a newly found performer to the table view.
+            If the function is not passed a UIImage to use as a performer icon, a default will be used.
+        
+            Returns true if the performer is successfully added, otherwise returns false. */
+        
+        return true | false
+    }
+    
     /*
     // MARK: - Navigation
 
