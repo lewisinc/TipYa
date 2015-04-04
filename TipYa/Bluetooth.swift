5813fdb1719c1b5b337b107b2d576d83f54f0c45
David Lewis <davidlewis@icloud.com>
2015-04-04 03:02:20 -0700

    Added groups for areas of interest, plus added the firebase framework to the project.

---
+++ TipYa/Bluetooth.swift
@@ -0,0 +1,10 @@
+//
+//  Bluetooth.swift
+//  TipYa
+//
+//  Created by David Lewis on 4/4/15.
+//  Copyright (c) 2015 LAHacksDLNSJO. All rights reserved.
+//
+
+import Foundation
+import CoreBluetooth
---
+++ TipYa/FirebaseUtility.swift
@@ -0,0 +1,11 @@
+//
+//  FirebaseUtility.swift
+//  TipYa
+//
+//  Created by David Lewis on 4/4/15.
+//  Copyright (c) 2015 LAHacksDLNSJO. All rights reserved.
+//
+
+import Foundation
+
+var rootReference = Firebase(url:"https://tipyalahacks2015.firebaseio.com")
