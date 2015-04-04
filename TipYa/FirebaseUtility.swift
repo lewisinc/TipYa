78af98b8774a40457ec8448e7b1f5b30e7b440c0
David Lewis <davidlewis@icloud.com>
2015-04-04 13:17:17 -0700

    Added encapsulating class.

--- TipYa/FirebaseUtility.swift
+++ TipYa/FirebaseUtility.swift
@@ -10,9 +10,12 @@ import Foundation
 
 var rootReference = Firebase(url:"https://tipyalahacks2015.firebaseio.com")
 
-// Attach a closure to read the data at our posts reference
-rootReference.observeEventType(.Value, withBlock: { snapshot in
-    println(snapshot.value)
-    }, withCancelBlock: { error in
-        println(error.description)
-})
+
+class FirebaseUtility {
+    // Attach a closure to read the data at our posts reference
+    rootReference.observeEventType(.Value, withBlock: { snapshot in
+        println(snapshot.value)
+        }, withCancelBlock: { error in
+            println(error.description)
+    })
+}
