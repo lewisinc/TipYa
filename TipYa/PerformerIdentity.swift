bc90afa8add5d719f2ee2673074992d4f753f82d
David Lewis <davidlewis@icloud.com>
2015-04-04 07:59:29 -0700

    Continuing work on Bluetooth

--- TipYa/PerformerIdentity.swift
+++ TipYa/PerformerIdentity.swift
@@ -7,7 +7,20 @@
 //
 
 import UIKit
+import CoreBluetooth
 
 class PerformerIdentity: NSObject {
-   
+    var name:String?
+    var image:UIImage?
+    var bioText:String?
+    var identityKey:CBUUID?
+    
+    init(name:String!, image:UIImage!, text:String!, uuidKey:CBUUID!) {
+        super.init()
+        self.name = name
+        self.image = image
+        self.bioText = text
+        self.identityKey = uuidKey
+    }
+    
 }
