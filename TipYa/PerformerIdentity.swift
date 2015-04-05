35d17d371df8a2ae1247a2acf8b7bc78c07faf0b
David Lewis <davidlewis@icloud.com>
2015-04-04 23:28:21 -0700

    Changed the Performer Identity file, added some cool changes. Yee haw!

--- TipYa/PerformerIdentity.swift
+++ TipYa/PerformerIdentity.swift
@@ -11,18 +11,32 @@ import CoreBluetooth
 import Security
 
 class PerformerIdentity: NSObject {
-    var name:String?
-    var passHash:String?
-    var image:UIImage?
-    var bioText:String?
+    var name:String?            // Name
+    var image:UIImage?          // Image
+    var bioText:String?         // Biography
+    var facebookLink:String?    // Facebook
+    var youtubeLink:String?     // Youtube
+    var miscWebsite:String?     // Other Miscellaneous Website
     var identityKey:CBUUID?
     
-    init(name:String!, image:UIImage!, text:String!, uuidKey:CBUUID!) {
+    
+    init(name:String?, image:UIImage?, text:String?, facebook:String?, youtube:String?, otherWebsite:String?, idKey:CBUUID?) {
         super.init()
         self.name = name
         self.image = image
         self.bioText = text
-        self.identityKey = uuidKey
+        self.facebookLink = facebook
+        self.youtubeLink = youtube
+        self.miscWebsite = otherWebsite
+        self.identityKey = idKey
     }
+    
+    // IN THE FUTURE
+    /*
+        It will be possible to save a users data (not another user's) to NSUserDefaults for persistence across relaunches.
+    
+    func saveToNSUserDefaults(identity:PerformerIdentity!)
+    func loadFromNSUserDefaults()
+    */
     
 }
